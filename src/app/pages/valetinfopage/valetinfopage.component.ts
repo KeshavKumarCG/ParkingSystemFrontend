25e9a26fe21ea044ea38ee942515cf10352502d7
Harshdeep Singh <harshdeep.singh@cginfinity.com>
2024-10-11 09:35:31 +0530

    Added SVG CGInfinity logo to the navbar and login page, Center algned the login page

--- src/app/pages/valetinfopage/valetinfopage.component.ts
+++ src/app/pages/valetinfopage/valetinfopage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-valetinfopage',
   standalone: true,
-  imports: [],
+  imports: [  NavbarComponent ],
   templateUrl: './valetinfopage.component.html',
   styleUrl: './valetinfopage.component.css'
 })
