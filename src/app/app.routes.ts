50d440f017e8b1e7f9e05a57644cc77c403876a6
Harshdeep Singh <harshdeep.singh@cginfinity.com>
2024-10-15 12:08:56 +0530

    Removed Home page and Home route from the navbar. Set Login page as the default landing page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { UserHomePageComponent } from './pages/user-home-page/user-home-page.component';
-import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 import { ValetinfopageComponent } from './pages/valetinfopage/valetinfopage.component';
 
 export const routes: Routes = [
-    { path: '', component: WelcomePageComponent },  // Default route
+    { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect root path to /login
     { path: 'login', component: LoginComponent },
     { path: 'user/home', component: UserHomePageComponent },
     { path: 'valet/info', component: ValetinfopageComponent },
