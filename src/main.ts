6aa08dcc0ca041a8a91f4796929089eb63f17367
KeshavKumarCG <keshav.kumar@cginfinity.com>
2024-10-16 17:14:13 +0530

    redirection and auth guard enabled

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
