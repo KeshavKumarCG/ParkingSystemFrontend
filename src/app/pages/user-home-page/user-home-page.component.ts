e1eb719aff0641123cb8e917ee5319c807c95ce3
GursimranKaurCG <Gursimran.Kaur@cginfinity.com>
2024-10-16 12:04:40 +0530

    Refactor user home page component styles and template
    - Clean up CSS styles in user-home-page.component.css
    - Update HTML structure and classes in user-home-page.component.html
    - Add modal functionality to user-home-page.component.ts

--- src/app/pages/user-home-page/user-home-page.component.ts
+++ src/app/pages/user-home-page/user-home-page.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../components/navbar/navbar.component'; // Import the standalone component
 
 @Component({
   selector: 'app-user-home-page',
-  standalone: true,
-  imports: [NavbarComponent],
+  standalone: true, // Define this as a standalone component
+  imports: [CommonModule, NavbarComponent], // Import the AppNavbarComponent
   templateUrl: './user-home-page.component.html',
-  styleUrl: './user-home-page.component.css'
+  styleUrls: ['./user-home-page.component.css']
 })
 export class UserHomePageComponent {
+  showModal: boolean = false;
 
+  // Method to open the modal
+  openModal() {
+    this.showModal = true;
+  }
+
+  // Method to close the modal
+  closeModal() {
+    this.showModal = false;
+  }
 }
