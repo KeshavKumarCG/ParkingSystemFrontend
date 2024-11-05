b6c4bf156c5d5d5e55fb5ac49f1c62cffc375097
Harshdeep Singh <harshdeep.singh@cginfinity.com>
2024-11-05 15:17:18 +0530

    Add delay to login response handling for improved user experience

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -46,7 +46,7 @@ export class LoginComponent {
             } else if (response.role === 'Valet') {
               this.router.navigate([`/valet/home/${response.id}`]);
             }
-          }); 
+          },1000); 
         } else {
           Toastify({
             text: "Login failed: Invalid response",
