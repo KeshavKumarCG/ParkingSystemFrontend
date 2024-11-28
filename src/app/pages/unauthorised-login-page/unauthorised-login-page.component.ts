ffca8356c1f8087e4d7cfbfbbcec893974ff15b7
KeshavKumarCG <keshav.kumar@cginfinity.com>
2024-11-28 14:14:33 +0530

    Remove unused import of UnauthorisedLoginPageComponent in unauthorised-login-page.component.ts

--- src/app/pages/unauthorised-login-page/unauthorised-login-page.component.ts
+++ src/app/pages/unauthorised-login-page/unauthorised-login-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-unauthorised-login-page',
   standalone: true,
-  imports: [UnauthorisedLoginPageComponent],
   templateUrl: './unauthorised-login-page.component.html',
   styleUrl: './unauthorised-login-page.component.scss'
 })
